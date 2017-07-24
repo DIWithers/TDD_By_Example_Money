f22a9fb26a6a7323e0a130caf2199625e363557c
DIWithers <d.ivy.withers@gmail.com>
2017-07-24 12:39:29 -0400

    2 Degenerate Objects - original dollar no longer changed with multiplication

--- spec/money.spec.ts
+++ spec/money.spec.ts
@@ -3,8 +3,12 @@ import {Dollar} from "../src/money";
 describe("Money Example", () => {
     it("should test multiplication", () => {
         let five: Dollar = new Dollar(5);
-        five.times(2);
-        expect(five.amount).toBe(10);
+        let product: Dollar = five.times(2);
+        expect(product.amount).toBe(10);
+
+        product = five.times(3);
+        expect(product.amount).toBe(15);
+
     });
 
 });
