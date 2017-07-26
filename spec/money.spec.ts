635cf3b207831f38b6e7a744cef14c44caa5fb77
DIWithers <d.ivy.withers@gmail.com>
2017-07-26 09:52:37 -0400

    ToEqual more appropriate than ToBe

--- spec/money.spec.ts
+++ spec/money.spec.ts
@@ -4,10 +4,10 @@ describe("Money Example", () => {
     it("should test multiplication", () => {
         let five: Dollar = new Dollar(5);
         let product: Dollar = five.times(2);
-        expect(product.amount).toBe(10);
+        expect(product.amount).toEqual(10);
 
         product = five.times(3);
-        expect(product.amount).toBe(15);
+        expect(product.amount).toEqual(15);
 
     });
 
